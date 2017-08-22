e7022c0330274f8548b1df839bbefa096a64bde1
r.khavronenko <r.khavronenko@vertamedia.com>
2017-08-22 15:08:46 +0300

    add timeout for clickhouse query

--- utils.go
+++ utils.go
@@ -5,16 +5,22 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/hagen1778/chproxy/log"
 )
 
+var client = &http.Client{
+	Timeout: time.Second * 60,
+}
+
 func doQuery(query, addr string) error {
-	resp, err := http.Post(fmt.Sprintf("%s/?query=%s", addr, url.QueryEscape(query)), "", nil)
+	resp, err := client.Post(fmt.Sprintf("%s/?query=%s", addr, url.QueryEscape(query)), "", nil)
 	if err != nil {
-		return fmt.Errorf("error in clickhouse query %q at %q: %s", query, addr, err)
+		return fmt.Errorf("error while executing clickhouse query %q at %q: %s", query, addr, err)
 	}
 	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		responseBody, _ := ioutil.ReadAll(resp.Body)
 		return fmt.Errorf("unexpected status code returned from query %q at %q: %d. Expecting %d. Response body: %q",
