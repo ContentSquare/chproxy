ContentSquare/chproxy

commit 81ec7a1bbf040b22866bdfaa1f4905dbde56e6f6

Add `max_queue_size` and `max_queue_time` options on user level in order to allow delaying request execution if it exceeds the current limits

scope.go
@@ -39,6 +39,70 @@ type scope struct {
 
 var scopeID = uint64(time.Now().UnixNano())
 
+func (s *scope) incQueued() error {
+	select {
+	case s.user.queueCh <- struct{}{}:
+		userLabels := prometheus.Labels{
+			"user": s.user.name,
+		}
+		requestQueueSizes.With(userLabels).Inc()
+		defer func() {
+			<-s.user.queueCh
+			requestQueueSizes.With(userLabels).Dec()
+		}()
+	default:
+		// Request queue is full. Give the request
+		// a single chance to run.
+		return s.inc()
+	}
+
+	// The request has been successfully queued.
+	// Try starting it during the given duration.
+	d := s.user.maxQueueTime
+	if d <= 0 {
+		// Default queue time.
+		d = 10 * time.Second
+	}
+	dSleep := d / 10
+	if dSleep > time.Second {
+		dSleep = time.Second
+	}
+	if dSleep < time.Millisecond {
+		dSleep = time.Millisecond
+	}
+	deadline := time.Now().Add(d)
+	for {
+		err := s.inc()
+		if err == nil {
+			// The request is allowed to start.
+			return nil
+		}
+
+		dLeft := time.Until(deadline)
+		if dLeft <= 0 {
+			// Give up: the request exceeded its wait time
+			// in the queue :(
+			return err
+		}
+
+		// The request has dLeft remaining time to wait in the queue.
+		// Sleep for a bit and try starting it again.
+		if dSleep > dLeft {
+			time.Sleep(dLeft)
+		} else {
+			time.Sleep(dSleep)
+		}
+
+		// Choose new host, since the previous one may become obsolete
+		// after sleeping.
+		h := s.cluster.getHost()
+		if h != nil {
+			s.host = h
+			s.labels["cluster_node"] = h.addr.Host
+		}
+	}
+}
+
 func (s *scope) inc() error {
 	uQueries := s.user.queryCounter.inc()
 	cQueries := s.clusterUser.queryCounter.inc()
@@ -195,11 +259,13 @@ type user struct {
 
 	name, password       string
 	maxExecutionTime     time.Duration
+	maxQueueTime         time.Duration
 	maxConcurrentQueries uint32
 	reqPerMin            uint32
 
 	rateLimiter  rateLimiter
 	queryCounter counter
+	queueCh      chan struct{}
 }
 
 type clusterUser struct {

config/config.go
@@ -347,6 +347,15 @@ type User struct {
 	// if omitted or zero - no limits would be applied
 	ReqPerMin uint32 `yaml:"requests_per_minute,omitempty"`
 
+	// Maximum number of queries waiting for execution in the queue
+	// if omitted or zero - queries are executed without waiting
+	// in the queue
+	MaxQueueSize uint32 `yaml:"max_queue_size,omitempty"`
+
+	// Maximum duration the query may wait in the queue
+	// if omitted or zero - 10s duration is used
+	MaxQueueTime time.Duration `yaml:"max_queue_time,omitempty"`
+
 	NetworksOrGroups NetworksOrGroups `yaml:"allowed_networks,omitempty"`
 
 	// List of networks that access is allowed from
@@ -388,6 +397,10 @@ func (u *User) UnmarshalYAML(unmarshal func(interface{}) error) error {
 
 	if u.DenyHTTP && u.DenyHTTPS {
 		return fmt.Errorf("user %q has both `deny_http` and `deny_https` set to `true`", u.Name)
+	}
+
+	if u.MaxQueueTime > 0 && u.MaxQueueSize == 0 {
+		return fmt.Errorf("`max_queue_size` must be set if `max_queue_time` is set on the user %q", u.Name)
 	}
 
 	return checkOverflow(u.XXX, "user")

config/config_test.go
@@ -80,13 +80,15 @@ func TestLoadConfig(t *testing.T) {
 				},
 				Users: []User{
 					{
-						Name:      "web",
-						Password:  "****",
-						ToCluster: "first cluster",
-						ToUser:    "web",
-						DenyHTTP:  true,
-						AllowCORS: true,
-						ReqPerMin: 4,
+						Name:         "web",
+						Password:     "****",
+						ToCluster:    "first cluster",
+						ToUser:       "web",
+						DenyHTTP:     true,
+						AllowCORS:    true,
+						ReqPerMin:    4,
+						MaxQueueSize: 100,
+						MaxQueueTime: 35 * time.Second,
 					},
 					{
 						Name:                 "default",
@@ -262,6 +264,11 @@ func TestBadConfig(t *testing.T) {
 			"network groups",
 			"testdata/bad.network_groups.yml",
 			"wrong network group name or address \"office\": invalid CIDR address: office/32",
+		},
+		{
+			"max queue size and time",
+			"testdata/bad.queue_size_time.yml",
+			"`max_queue_size` must be set if `max_queue_time` is set on the user \"default\"",
 		},
 	}
 

config/testdata/bad.queue_size_time.yml
@@ -1,0 +1,14 @@
+server:
+  http:
+    listen_addr: ":8080"
+    allowed_networks: ["127.0.0.1"]
+
+users:
+  - name: "default"
+    to_cluster: "cluster"
+    to_user: "default"
+    max_queue_time: 20s
+
+clusters:
+  - name: "cluster"
+    nodes: ["127.0.0.1:8123"]

config/testdata/full.yml
@@ -81,6 +81,22 @@ users:
     # Requests per minute limit for the given input user.
     requests_per_minute: 4
 
+    # The maximum number of requests that may wait for their chance
+    # to be executed because they cannot run now due to the current limits.
+    #
+    # This option may be useful for handling request bursts from `tabix`
+    # or `clickhouse-grafana`.
+    #
+    # By default all the requests are immediately executed without
+    # waiting in the queue.
+    max_queue_size: 100
+
+    # The maximum duration the queued requests may wait for their chance
+    # to be executed.
+    # This option makes sense only if max_queue_size is set.
+    # By default requests wait for up to 10 seconds in the queue.
+    max_queue_time: 35s
+
   - name: "default"
     to_cluster: "second cluster"
     to_user: "default"

metrics.go
@@ -55,9 +55,16 @@ var (
 	concurrentQueries = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "concurrent_queries",
-			Help: "Number of concurrent queries at current time",
+			Help: "The number of concurrent queries at current time",
 		},
 		[]string{"user", "cluster", "cluster_user", "cluster_node"},
+	)
+	requestQueueSizes = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "request_queue_sizes",
+			Help: "The size of per-user request queues at the current time",
+		},
+		[]string{"user"},
 	)
 	requestBodyBytes = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
@@ -81,6 +88,6 @@ var (
 
 func init() {
 	prometheus.MustRegister(statusCodes, requestDuration, requestSum, requestSuccess,
-		limitExcess, hostPenalties, hostHealth, concurrentQueries,
+		limitExcess, hostPenalties, hostHealth, concurrentQueries, requestQueueSizes,
 		requestBodyBytes, responseBodyBytes, badRequest)
 }

proxy.go
@@ -39,6 +39,13 @@ func (rp *reverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	log.Debugf("Request scope %s", s)
 	requestSum.With(s.labels).Inc()
 
+	if err := s.incQueued(); err != nil {
+		limitExcess.With(s.labels).Inc()
+		respondWith(rw, err, http.StatusTooManyRequests)
+		return
+	}
+	defer s.dec()
+
 	req.Body = &statReadCloser{
 		ReadCloser:       req.Body,
 		requestBodyBytes: requestBodyBytes.With(s.labels),
@@ -47,13 +54,6 @@ func (rp *reverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		ResponseWriter:    rw,
 		responseBodyBytes: responseBodyBytes.With(s.labels),
 	}
-
-	if err = s.inc(); err != nil {
-		limitExcess.With(s.labels).Inc()
-		respondWith(rw, err, http.StatusTooManyRequests)
-		return
-	}
-	defer s.dec()
 
 	if s.user.allowCORS {
 		origin := req.Header.Get("Origin")
@@ -68,7 +68,7 @@ func (rp *reverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	timeout, timeoutErrMsg := s.getTimeoutWithErrMsg()
 	ctx := context.Background()
-	if timeout != 0 {
+	if timeout > 0 {
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, timeout)
 		defer cancel()
@@ -182,6 +182,8 @@ func (rp *reverseProxy) ApplyConfig(cfg *config.Config) error {
 			allowedNetworks:      u.AllowedNetworks,
 			maxExecutionTime:     u.MaxExecutionTime,
 			maxConcurrentQueries: u.MaxConcurrentQueries,
+			maxQueueTime:         u.MaxQueueTime,
+			queueCh:              make(chan struct{}, u.MaxQueueSize),
 		}
 	}
 

commit 3c3de006fbcc3b17de53810c3e96c21ac2beddc6

Fix `requests_per_minute` enforcing. It has been broken in the commit 81ec7a1bbf040b22866bdfaa1f4905dbde56e6f6. Added clarification comments in scope.inc/scope.dec

scope.go
@@ -106,10 +106,6 @@ func (s *scope) incQueued() error {
 func (s *scope) inc() error {
 	uQueries := s.user.queryCounter.inc()
 	cQueries := s.clusterUser.queryCounter.inc()
-	uRPM := s.user.rateLimiter.inc()
-	cRPM := s.clusterUser.rateLimiter.inc()
-	s.host.inc()
-	concurrentQueries.With(s.labels).Inc()
 
 	var err error
 	if s.user.maxConcurrentQueries > 0 && uQueries > s.user.maxConcurrentQueries {
@@ -121,8 +117,11 @@ func (s *scope) inc() error {
 			s.clusterUser.name, s.clusterUser.maxConcurrentQueries)
 	}
 
+	uRPM := s.user.rateLimiter.inc()
+	cRPM := s.clusterUser.rateLimiter.inc()
+
 	// int32(xRPM) > 0 check is required to detect races when RPM
-	// is decremented in scope.dec after per-minute zeroing
+	// is decremented on error below after per-minute zeroing
 	// in rateLimiter.run.
 	// These races become innocent with the given check.
 	if s.user.reqPerMin > 0 && int32(uRPM) > 0 && uRPM > s.user.reqPerMin {
@@ -135,17 +134,27 @@ func (s *scope) inc() error {
 	}
 
 	if err != nil {
-		s.dec()
+		s.user.queryCounter.dec()
+		s.clusterUser.queryCounter.dec()
+
+		// Decrement rate limiter here, so it doesn't count requests
+		// that didn't start due to limits overflow.
+		s.user.rateLimiter.dec()
+		s.clusterUser.rateLimiter.dec()
 		return err
 	}
+
+	s.host.inc()
+	concurrentQueries.With(s.labels).Inc()
 	return nil
 }
 
 func (s *scope) dec() {
+	// There is no need in ratelimiter.dec here, since the rate limiter
+	// is automatically zeroed every minute in rateLimiter.run.
+
 	s.user.queryCounter.dec()
 	s.clusterUser.queryCounter.dec()
-	s.user.rateLimiter.dec()
-	s.clusterUser.rateLimiter.dec()
 	s.host.dec()
 	concurrentQueries.With(s.labels).Dec()
 }