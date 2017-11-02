492b27c2f047e2c2c528b9c52c2daa87069c1243
Aliaksandr Valialkin <valyala@gmail.com>
2017-11-02 13:54:37 +0200

    there is no need in creating cache directory, since it is automatically created during cache open

--- main_test.go
+++ main_test.go
@@ -22,9 +22,6 @@ import (
 var testDir = "./temp-test-data"
 
 func TestMain(m *testing.M) {
-	if _, err := os.Stat(testDir); os.IsNotExist(err) {
-		os.Mkdir(testDir, os.ModePerm)
-	}
 	log.SuppressOutput(true)
 	retCode := m.Run()
 	log.SuppressOutput(false)
