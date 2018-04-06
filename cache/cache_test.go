0ad6429a649eac3e8cec3391487ba082bccd6bc7
hagen1778 <hagen1778@gmail.com>
2018-04-06 18:19:56 +0300

    support enable_http_compression param

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -48,14 +48,14 @@ func TestKeyString(t *testing.T) {
 			key: &Key{
 				Query: []byte("SELECT 1 FROM system.numbers LIMIT 10"),
 			},
-			expected: "973507f73eb5662679a62926a38a8b7d",
+			expected: "cd8ba039ef5df8287ff91a3907e21fcf",
 		},
 		{
 			key: &Key{
 				Query:          []byte("SELECT 1 FROM system.numbers LIMIT 10"),
 				AcceptEncoding: "gzip",
 			},
-			expected: "e48ef09b985141e189ee0a0ba8b94cfb",
+			expected: "e30d9db1d8f6a0844b7c6047d14a6c8b",
 		},
 		{
 			key: &Key{
@@ -63,7 +63,7 @@ func TestKeyString(t *testing.T) {
 				AcceptEncoding: "gzip",
 				DefaultFormat:  "JSON",
 			},
-			expected: "282962400943b25a255e2dbb65d79e87",
+			expected: "70bd83ab8def6df2c65214b37410050c",
 		},
 		{
 			key: &Key{
@@ -72,7 +72,7 @@ func TestKeyString(t *testing.T) {
 				DefaultFormat:  "JSON",
 				Database:       "foobar",
 			},
-			expected: "04bafec4d4202e8ba286d510cf3e5827",
+			expected: "3fb481313cd5a0061832de383183ac3a",
 		},
 		{
 			key: &Key{
@@ -82,7 +82,7 @@ func TestKeyString(t *testing.T) {
 				Database:       "foobar",
 				Namespace:      "ns123",
 			},
-			expected: "0b3e365eeaeedb0a217ff89c989b79d6",
+			expected: "556f114207e2f8bc4cbd574b5035b25e",
 		},
 		{
 			key: &Key{
@@ -93,7 +93,7 @@ func TestKeyString(t *testing.T) {
 				Compress:       "1",
 				Namespace:      "ns123",
 			},
-			expected: "fab445ab05b208016ac07679b4224351",
+			expected: "5fe3d904e7e76c5fe7ba617567a4cb7a",
 		},
 	}
 
