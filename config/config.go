a5f1614c06e57f4fbfff77a1823b02bf3fd4b4a7
Paweł Gontarz <garnek522@gmail.com>
2022-07-13 18:32:57 +0200

    doc(max_exec_time): set by default to 30s (#186)

--- config/config.go
+++ config/config.go
@@ -479,7 +479,7 @@ type User struct {
 	MaxConcurrentQueries uint32 `yaml:"max_concurrent_queries,omitempty"`
 
 	// Maximum duration of query execution for user
-	// if omitted or zero - no limits would be applied
+	// if omitted or zero - limit is set to 30 seconds
 	MaxExecutionTime Duration `yaml:"max_execution_time,omitempty"`
 
 	// Maximum number of requests per minute for user
@@ -720,7 +720,7 @@ type ClusterUser struct {
 	MaxConcurrentQueries uint32 `yaml:"max_concurrent_queries,omitempty"`
 
 	// Maximum duration of query execution for user
-	// if omitted or zero - no limits would be applied
+	// if omitted or zero - limit is set to 30 seconds
 	MaxExecutionTime Duration `yaml:"max_execution_time,omitempty"`
 
 	// Maximum number of requests per minute for user
