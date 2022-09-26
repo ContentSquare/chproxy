67f163eb2e2b96fa2e31f33ff7fb015c5379eb22
Pawel Gontarz <pawel.gontarz@contentsquare.com>
2022-09-26 18:50:24 +0200

    chore: add test over cleanup of completed transaction

--- cache/transaction_registry_redis_test.go
+++ cache/transaction_registry_redis_test.go
@@ -80,7 +80,7 @@ func TestFailRedisTransaction(t *testing.T) {
 	}
 }
 
-func TestCleanupRedisTransaction(t *testing.T) {
+func TestCleanupFailedRedisTransaction(t *testing.T) {
 	s := miniredis.RunT(t)
 
 	redisClient := redis.NewUniversalClient(&redis.UniversalOptions{
@@ -117,6 +117,48 @@ func TestCleanupRedisTransaction(t *testing.T) {
 
 	if status.FailReason != failReason {
 		t.Fatalf("unexpected: transaction should curry fail reason")
+	}
+
+	// move ttls of mini redis to trigger the clean up transaction
+	s.FastForward(updatedTTL)
+
+	status, err = redisTransaction.Status(key)
+	if err != nil || !status.State.IsAbsent() {
+		t.Fatalf("unexpected: transaction should be cleaned up")
+	}
+}
+
+func TestCleanupCompletedRedisTransaction(t *testing.T) {
+	s := miniredis.RunT(t)
+
+	redisClient := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs: []string{s.Addr()},
+	})
+
+	graceTime := 10 * time.Second
+	updatedTTL := 10 * time.Millisecond
+	key := &Key{
+		Query: []byte("SELECT pending entries"),
+	}
+
+	redisTransaction := newRedisTransactionRegistry(redisClient, graceTime, updatedTTL)
+
+	if err := redisTransaction.Create(key); err != nil {
+		t.Fatalf("unexpected error: %s while registering new transaction", err)
+	}
+
+	status, err := redisTransaction.Status(key)
+	if err != nil || !status.State.IsPending() {
+		t.Fatalf("unexpected: transaction should be pending")
+	}
+
+	if err := redisTransaction.Complete(key); err != nil {
+		t.Fatalf("unexpected error: %s while unregistering transaction", err)
+	}
+
+	status, err = redisTransaction.Status(key)
+	if err != nil || !status.State.IsCompleted() {
+		t.Fatalf("unexpected: transaction should be failed")
 	}
 
 	// move ttls of mini redis to trigger the clean up transaction
