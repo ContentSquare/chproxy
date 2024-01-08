735847bc6bd7ff8908fd5a54693a2cf923e483af
k.torgaev <k.torgaev@tinkoff.ru>
2024-01-08 07:27:13 +0300

    fix tests

--- config/config_test.go
+++ config/config_test.go
@@ -262,7 +262,8 @@ var fullConfig = Config{
 			},
 		},
 	},
-	networkReg: map[string]Networks{},
+	MaxErrorReasonSize: ByteSize(100 << 20),
+	networkReg:         map[string]Networks{},
 }
 
 func TestLoadConfig(t *testing.T) {
@@ -319,6 +320,7 @@ func TestLoadConfig(t *testing.T) {
 						MaxExecutionTime: Duration(120 * time.Second),
 					},
 				},
+				MaxErrorReasonSize: ByteSize(1 << 50),
 			},
 		},
 	}
@@ -880,6 +882,7 @@ network_groups:
 - name: reporting-apps
   networks:
   - 10.10.10.0/24
+max_error_reason_size: 104857600
 caches:
 - mode: file_system
   name: longterm
